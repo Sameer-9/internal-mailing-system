e8da88860af4841d3991f6e817cc28d4cfedcc42
Sameer-0 <sameershaikhx123@gmail.com>
2023-04-25 00:34:02 +0530

    realtime done

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			strategies: 'injectManifest',
-			srcDir: 'src',
-			filename: 'serviceWorker.ts'
-			/* other pwa options */
-		})
+		sveltekit()
 	]
 });
